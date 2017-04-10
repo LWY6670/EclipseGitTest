363e78c23fce1e72f4f37f9e22ed33304f3b1722
Li Weiyuan <lwy6670@163.com>
2017-04-11 01:04:15 +0800

    新Share Project之后首次提交

---
+++ src/com/liwy/cn/HelloWorld001.java
@@ -0,0 +1,10 @@
+package com.liwy.cn;
+
+public class HelloWorld001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Hello world 001");
+	}
+
+}
