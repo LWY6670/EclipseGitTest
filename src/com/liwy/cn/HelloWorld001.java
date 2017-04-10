3eb43ecaef50ec9d8b62ddeb9ae3a83eb253b5a7
Li Weiyuan <lwy6670@163.com>
2017-04-11 01:29:33 +0800

    Hello world 001 修改1

--- src/com/liwy/cn/HelloWorld001.java
+++ src/com/liwy/cn/HelloWorld001.java
@@ -4,7 +4,7 @@ public class HelloWorld001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.print("Hello world 001");
+        System.out.print("Hello world 001 ÐÞ¸Ä1");
 	}
 
 }
